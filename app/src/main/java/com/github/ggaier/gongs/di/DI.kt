972846814e708f21be8254772d7698c59e489386
ggaier <jwenbo52@gmail.com>
2018-11-06 15:00:22 +0800

    add okhttp-logging-intercepter

--- app/src/main/java/com/github/ggaier/gongs/di/DI.kt
+++ app/src/main/java/com/github/ggaier/gongs/di/DI.kt
@@ -5,6 +5,8 @@ import com.github.ggaier.gongs.data.CollectionRepository
 import com.github.ggaier.gongs.data.local.CollectionLocalSource
 import com.github.ggaier.gongs.data.remote.CollectionRemoteSource
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -18,6 +20,9 @@ object DI {
         Retrofit.Builder().baseUrl("https://musicbrainz.org/ws/2/")
             .addCallAdapterFactory(CoroutineCallAdapterFactory())
             .addConverterFactory(GsonConverterFactory.create())
+            .client(OkHttpClient.Builder().addInterceptor(HttpLoggingInterceptor().also {
+                it.level = HttpLoggingInterceptor.Level.BASIC
+            }).build())
             .build()
             .create(MusicBrainzApi::class.java)
     }
