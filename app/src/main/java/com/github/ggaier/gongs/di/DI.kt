fd9328a92700ae510da055e8f9c245273f17eeea
ggaier <jwenbo52@gmail.com>
2018-11-06 14:53:29 +0800

    add gson converter, and internet permission

--- app/src/main/java/com/github/ggaier/gongs/di/DI.kt
+++ app/src/main/java/com/github/ggaier/gongs/di/DI.kt
@@ -6,6 +6,7 @@ import com.github.ggaier.gongs.data.local.CollectionLocalSource
 import com.github.ggaier.gongs.data.remote.CollectionRemoteSource
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 /**
  * Created by wenbo, 2018/10/25
@@ -16,6 +17,7 @@ object DI {
     private val musicBrainzApi by lazy {
         Retrofit.Builder().baseUrl("https://musicbrainz.org/ws/2/")
             .addCallAdapterFactory(CoroutineCallAdapterFactory())
+            .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(MusicBrainzApi::class.java)
     }
